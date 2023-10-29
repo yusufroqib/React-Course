13530a565a36f8260a5f8a31a7634f8c3b339502
yusufroqib <yusufroqib@gmail.com>
2023-10-30 00:53:22 +0100

    update

--- Lesson 05 (Form)/src/AddItem.jsx
+++ Lesson 05 (Form)/src/AddItem.jsx
@@ -9,15 +9,21 @@ const AddItem = ({newItem, setNewItem, handleSubmit}) => {
     <form className='addForm' onSubmit={handleSubmit}>
       <label htmlFor="addItem">Add Item</label>
       <input id='addItem'
-            autoFocus
-            ref={inputRef} 
-            type='text' 
-            placeholder='Add Item' 
-            required 
-            value={newItem}     //To allow setNewItem effect when input field is cleared
-            onChange={(e) => setNewItem(e.target.value)} 
+        autoFocus
+        ref={inputRef} 
+        type='text' 
+        placeholder='Add Item' 
+        required 
+        value={newItem}     //To allow setNewItem effect when input field is cleared
+        onChange={(e) => setNewItem(e.target.value)} 
       />
-      <button type='submit' aria-label='Add Item'> <FaPlus /> </button>
+      <button 
+        type='submit' 
+        aria-label='Add Item' 
+        onClick={() => inputRef.current.focus()}
+      > 
+        <FaPlus /> 
+      </button>
     </form>
   )
 }
