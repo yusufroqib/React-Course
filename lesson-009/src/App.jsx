3fd96553da8d7bb8d135ee8aa28d68754d7f76f5
yusufroqib <yusufroqib@gmail.com>
2023-10-30 16:16:56 +0100

    update

--- lesson-009/src/App.jsx
+++ lesson-009/src/App.jsx
@@ -73,9 +73,14 @@ function App() {
     if(result) setFetchError(result)
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id)
-    setItems(listItems)
+    setItems(listItems);
+
+    const deleteOptions = { method: 'DELETE'}
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, deleteOptions)
+    if(result) setFetchError(result)
   }
 
   const handleSubmit = (e) => {
