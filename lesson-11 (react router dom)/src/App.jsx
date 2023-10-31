771b31a156585a5c890e409293cffea863748c43
yusufroqib <yusufroqib@gmail.com>
2023-10-31 14:53:46 +0100

    update

--- lesson-11 (react router dom)/src/App.jsx
+++ lesson-11 (react router dom)/src/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Header from './Header'
+import Nav from './Nav'
+import Home from './Home'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import Missing from './Missing'
+import About from './About'
+import Footer from './Footer'
 
 const App = () => {
   return (
