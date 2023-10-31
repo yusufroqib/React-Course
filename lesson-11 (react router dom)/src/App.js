ec4882e59af87ee2f166dd163c68095e735cb8d2
yusufroqib <yusufroqib@gmail.com>
2023-10-31 14:48:05 +0100

    update

--- lesson-11 (react router dom)/src/App.js
+++ lesson-11 (react router dom)/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Header from './Header'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <Missing />
+      <About />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
