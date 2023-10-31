42a4050378b3d9c06a9880bb877ab066a064b130
yusufroqib <yusufroqib@gmail.com>
2023-10-31 16:12:16 +0100

    update

--- lesson-11 (react router dom)/src/App.jsx
+++ lesson-11 (react router dom)/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Header from './Header'
 import Nav from './Nav'
@@ -10,6 +10,36 @@ import About from './About'
 import Footer from './Footer'
 
 const App = () => {
+
+  const [search, setSearch] = useState('')
+  const [post, setPost] = useState([
+    {
+      id: 1,
+      title: 'Rocco',
+      date: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet corrupti facere dignissimos laborum! Numquam autem inventore vel architecto nam iure id placeat? Iste, maxime nisi.'
+    },
+    {
+      id: 2,
+      title: 'Kanas',
+      date: 'September 21, 2026 12:19:36 AM',
+      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet c.'
+    },
+    {
+      id: 3,
+      title: 'Muhammed',
+      date: 'July 01, 2024 11:55:36 AM',
+      body: ' consectetur adipisicing elit. Amet corrupti facere dignissimos laborum! Numquam autem inventore vel architecto nam iure id placeat? Iste, '
+    },
+    {
+      id: 4,
+      title: 'Supreme',
+      date: 'August 03, 2025 13:42:36 AM',
+      body: ' dignissimos laborum! Numquam autem inventore vel architecto nam consectetur adipisicing elit. Amet corrupti facere  iure id placeat? Iste, '
+    }
+  ])
+  const [searchResult, setSearchResult] = useState([])
+
   return (
     <div className='App'>
       <Header title= 'DLT Student Blog' />
