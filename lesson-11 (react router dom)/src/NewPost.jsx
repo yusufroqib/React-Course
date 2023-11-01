b04fb51dcc820c20a49dd7f21fb9ba7df6a62cbd
yusufroqib <yusufroqib@gmail.com>
2023-11-01 14:40:55 +0100

    update

--- lesson-11 (react router dom)/src/NewPost.jsx
+++ lesson-11 (react router dom)/src/NewPost.jsx
@@ -3,20 +3,20 @@ const NewPost = ({ postTitle, setPostTitle, postBody, setPostBody, handleSubmit 
     <main className="NewPost">
       <h2>New Post</h2>
       <form className="newPostForm" onSubmit={handleSubmit}>
-      <label htmlFor="postTitle">Post Title</label>
-      <input 
-        type="text" 
-        required
-        value={postTitle}
-        onChange={(e) => setPostTitle(e.target.value)}
-      />
-      <label htmlFor="postBody">Post Body</label>
-      <textarea 
-        id="postBody" 
-        required value={postBody} 
-        onChange={(e) => setPostBody(e.target.value)} 
+        <label htmlFor="postTitle">Post Title</label>
+        <input 
+          type="text" 
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
         />
-      <button type="submit">Submit</button>
+        <label htmlFor="postBody">Post Body</label>
+        <textarea 
+          id="postBody" 
+          required value={postBody} 
+          onChange={(e) => setPostBody(e.target.value)} 
+          />
+        <button type="submit">Submit</button>
       </form>
     </main>
   )
