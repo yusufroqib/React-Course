43e575a8297c4b8e8a9e079191cfd2256fc931c1
yusufroqib <yusufroqib@gmail.com>
2023-11-02 15:03:41 +0100

    update

--- lesson-12 react-router-dom-withAxios-andEdit/src/PostPage.jsx
+++ lesson-12 react-router-dom-withAxios-andEdit/src/PostPage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 
 const PostPage = ({posts, handleDelete}) => {
@@ -11,7 +11,10 @@ const PostPage = ({posts, handleDelete}) => {
         <h2 className="postTitle">{post.title}</h2>
         <p className="postDate">{post.date}</p>
         <p className="postBody">{post.body}</p>
-        <button onClick={ () => handleDelete(post.id)}>Delete</button>
+        <Link to={`/edit/${post.id}`}>
+          <button className="editButton">Edit Post</button>
+        </Link>
+        <button className="deleteButton" onClick={ () => handleDelete(post.id)}>Delete</button>
         </>
 
         }
