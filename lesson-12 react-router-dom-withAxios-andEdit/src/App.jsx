0df8a078326f6c1f064bdb4f7d7d6bc060b967de
yusufroqib <yusufroqib@gmail.com>
2023-11-03 10:40:42 +0100

    update

--- lesson-12 react-router-dom-withAxios-andEdit/src/App.jsx
+++ lesson-12 react-router-dom-withAxios-andEdit/src/App.jsx
@@ -22,6 +22,8 @@ const App = () => {
   const [editTitle, setEditTitle] = useState("");
   const [editBody, setEditBody] = useState("");
 
+  const { width } = useWindowSize()
+
   useEffect(() => {
     const filterResult = posts.filter(
       (post) =>
@@ -99,7 +101,7 @@ const App = () => {
     <Routes>
       <Route
         path="/"
-        element={<HomeLayout search={search} setSearch={setSearch} />}
+        element={<HomeLayout search={search} setSearch={setSearch} width={ width }/>}
       >
         <Route index element={<Home posts={searchResult} />} />
         <Route path="/post">
--- lesson-12 react-router-dom-withAxios-andEdit/src/HomeLayout.jsx
+++ lesson-12 react-router-dom-withAxios-andEdit/src/HomeLayout.jsx
@@ -3,10 +3,10 @@ import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
-const HomeLayout = ({search, setSearch}) => {
+const HomeLayout = ({ search, setSearch, width }) => {
   return (
     <div className="App">
-      <Header title="DLT Student Blog" />
+      <Header title="DLT Student Blog" width={ width } />
       <Nav  search={search} setSearch={setSearch}  />
       <Outlet />
       <Footer />
