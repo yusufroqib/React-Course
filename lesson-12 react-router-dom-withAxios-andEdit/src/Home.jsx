ce7537144c14f497c3bed379a36dce3616d000ca
yusufroqib <yusufroqib@gmail.com>
2023-11-03 12:42:15 +0100

    update

--- lesson-12 react-router-dom-withAxios-andEdit/src/Home.jsx
+++ lesson-12 react-router-dom-withAxios-andEdit/src/Home.jsx
@@ -14,6 +14,8 @@ const Home = ({ posts, fetchError, isLoading}) => {
     <main className="Home">
       {isLoading && <p className="statusMsg">Loading posts...</p> }
       { !isLoading && fetchError && <p className="statusMsg" style={{color: "red"}}>{fetchError}</p> }
+      { !isLoading && !fetchError && (
+        posts.length ? <Feed posts = {posts} /> : <p className="statusMsg"> No post to display</p> )}
     </main>
   )
 }
