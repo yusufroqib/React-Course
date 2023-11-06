5d3262aaa0cd921d5583b0a54702ad9f129db4a7
yusufroqib <yusufroqib@gmail.com>
2023-11-06 16:00:45 +0100

    updates

--- lesson-13 (Data-Context)/src/EditPost.jsx
+++ lesson-13 (Data-Context)/src/EditPost.jsx
@@ -1,57 +1,78 @@
-import React, { useContext, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import DataContext from './context/DataContext';
-
-
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import DataContext from "./context/DataContext";
+import { format } from "date-fns";
+import api from "./api/posts";
 
 const EditPost = () => {
-  const {posts, handleEdit, editBody, editTitle, setEditBody, setEditTitle} = useContext(DataContext)
-    const { id } = useParams();
-    const post = posts.find(post => (post.id).toString() === id)
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
+  const { posts, setPosts } = useContext(DataContext);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
 
-    useEffect(() => {
-        if(post) {
-            setEditTitle(post.title)
-            setEditBody(post.body)
-        }
-    }, [post, setEditTitle, setEditBody])
+  const handleEdit = async (id) => {
+    const date = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, date, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(
+        posts.map((post) => (post.id === id ? { ...response.data } : post))
+      );
+      setEditTitle("");
+      setEditBody("");
+      navigate("/");
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
+  };
+
+  useEffect(() => {
+    if (post) {
+      setEditTitle(post.title);
+      setEditBody(post.body);
+    }
+  }, [post, setEditTitle, setEditBody]);
+
   return (
     <main className="NewPost">
-        {editTitle && 
-            <>
-            
-    <h2>Edit Post</h2>
-    <form className="newPostForm" onSubmit={(e) => e.preventDefault()}>
-      <label htmlFor="postTittle"> Edit Title </label>
-      <input
-        type="text"
-        required
-        value={editTitle}
-        onChange={(e) => setEditTitle(e.target.value)}
-      />
-      <label htmlFor="postBody"> Edit Body </label>
-      <textarea
-        id="postBody"
-        required
-        value={editBody}
-        onChange={(e) => setEditBody(e.target.value)}
-      ></textarea>
-      <button type="submit" onClick={() => handleEdit(post.id)}>Submit</button>
-    </form>
-            </>
-        }
+      {editTitle && (
+        <>
+          <h2>Edit Post</h2>
+          <form className="newPostForm" onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="postTittle"> Edit Title </label>
+            <input
+              type="text"
+              required
+              value={editTitle}
+              onChange={(e) => setEditTitle(e.target.value)}
+            />
+            <label htmlFor="postBody"> Edit Body </label>
+            <textarea
+              id="postBody"
+              required
+              value={editBody}
+              onChange={(e) => setEditBody(e.target.value)}
+            ></textarea>
+            <button type="submit" onClick={() => handleEdit(post.id)}>
+              Submit
+            </button>
+          </form>
+        </>
+      )}
 
-        {!editTitle && 
-            <>
-                <h2>Post Not Found</h2>
-                <p>Well, that's disappointing</p>
-                <p>
-                    <Link to='/'>Visit our website</Link>
-                </p>
-            </>
-        }
-  </main>
-  )
-}
+      {!editTitle && (
+        <>
+          <h2>Post Not Found</h2>
+          <p>Well, that's disappointing</p>
+          <p>
+            <Link to="/">Visit our website</Link>
+          </p>
+        </>
+      )}
+    </main>
+  );
+};
 
-export default EditPost
+export default EditPost;
--- lesson-13 (Data-Context)/src/context/DataContext.jsx
+++ lesson-13 (Data-Context)/src/context/DataContext.jsx
@@ -1,22 +1,11 @@
 import { createContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { format } from "date-fns";
-import useWindowSize from "../hooks/useWindowSize";
 import useAxiosFetch from "../hooks/useAxiosFetch";
-import api from "../api/posts";
-
 const DataContext = createContext({})
 
 export const DataProvider = ({ children }) => {
     const [posts, setPosts] = useState([]);
-
     const [search, setSearch] = useState("");
     const [searchResult, setSearchResult] = useState([]);
-    const [postTitle, setPostTitle] = useState("");
-    const [postBody, setPostBody] = useState("");
-    const [editTitle, setEditTitle] = useState("");
-    const [editBody, setEditBody] = useState("");
-    const { width } = useWindowSize();
     const { data, fetchError, isLoading } = useAxiosFetch(
       "http://localhost:3500/posts"
     );
@@ -32,61 +21,12 @@ export const DataProvider = ({ children }) => {
           post.title.toLowerCase().includes(search.toLowerCase())
       );
       setSearchResult(filterResult.reverse());
-    }, [posts, search]);
-
-    const navigate = useNavigate();
-
-    const handleDelete = async (id) => {
-      try {
-        await api.delete(`/posts/${id}`);
-        const postLists = posts.filter((post) => post.id !== id);
-        setPosts(postLists);
-        navigate("/");
-      } catch (error) {
-        console.log(`Error: ${error.message}`);
-      }
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-      const date = format(new Date(), "MMMM dd, yyyy pp");
-      const newPost = { id, title: postTitle, date, body: postBody };
-      try {
-        const response = await api.post("/posts", newPost);
-        const allPost = [...posts, response.data];
-        setPosts(allPost);
-        setPostTitle("");
-        setPostBody("");
-        navigate("/");
-      } catch (error) {
-        console.log(`Error: ${error.message}`);
-      }
-    };
-  
-    const handleEdit = async (id) => {
-      const date = format(new Date(), "MMMM dd, yyyy pp");
-      const updatedPost = { id, title: editTitle, date, body: editBody };
-      try {
-        const response = await api.put(`/posts/${id}`, updatedPost);
-        setPosts(
-          posts.map((post) => (post.id === id ? { ...response.data } : post))
-        );
-        setEditTitle("");
-        setEditBody("");
-        navigate("/");
-      } catch (error) {
-        console.log(`Error: ${error.message}`);
-      }
-    };
+    }, [posts, search]);  
 
     return (
         <DataContext.Provider value={{
-            width, search, setSearch,    //header and nav
-            posts, fetchError, isLoading, searchResult,  //Home
-            postTitle, setPostTitle, postBody, setPostBody, handleSubmit, //NewPost
-            handleDelete, //PostPage
-            setEditBody, editBody, editTitle, setEditTitle, handleEdit //EditPost
+            search, setSearch,    //header and nav
+            posts, fetchError, isLoading, searchResult, setPosts, //Home
         }}>
             {children}
         </DataContext.Provider>
