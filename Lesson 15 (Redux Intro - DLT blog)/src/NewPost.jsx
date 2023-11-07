2b01fe9fdd6b5ca17ef126a5dafb31b08ebe1c07
yusufroqib <yusufroqib@gmail.com>
2023-11-07 14:25:07 +0100

    update

--- Lesson 15 (Redux Intro - DLT blog)/src/NewPost.jsx
+++ Lesson 15 (Redux Intro - DLT blog)/src/NewPost.jsx
@@ -1,16 +1,23 @@
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
+import { useStoreActions, useStoreState } from "easy-peasy";
 
 const NewPost = () => {
   const navigate = useNavigate();  
+  const posts = useStoreState((state) => state.posts);
+  const postTitle = useStoreState((state) => state.postTitle);
+  const postBody = useStoreState((state) => state.postBody);
+  const savePost = useStoreActions((action) => action.savePost)
+  const setPostTitle = useStoreActions((action) => action.setPostTitle)
+  const setPostBody = useStoreActions((action) => action.setPostBody)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const date = format(new Date(), "MMMM dd, yyyy pp");
     const newPost = { id, title: postTitle, date, body: postBody };
+    savePost(newPost)
     navigate("/");
-
   };
 
   return (
