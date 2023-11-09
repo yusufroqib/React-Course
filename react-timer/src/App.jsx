937a75b8b1d6ac251be84b8b5024ece695365861
yusufroqib <yusufroqib@gmail.com>
2023-11-09 11:43:59 +0100

    update

--- react-timer/src/App.jsx
+++ react-timer/src/App.jsx
@@ -1,27 +1,11 @@
-import { useState, useEffect, useCallback, useMemo } from 'react'
+import { useState, useEffect } from 'react'
 
-const getArray = () => {
-  for (let i = 0; i < 1000000000; i++){
-
-  }
-  return ['Muhammad', "Fine Boy"]
-}
 
 const App = () => {
   const [userInput, setUserInput] = useState("")
   const [randomInput, setRandomInput] = useState("")
 
-  const fib = useCallback((n) => {
-    return n <= 1 ? n : fib(n - 1) + fib(n-2)
-  }, [])
 
-  const fibNumber = useMemo(() => fib(userInput), [userInput, fib])
-
-  const myArray = useMemo(() => getArray(), [])
-
-  useEffect(() => {
-    console.log("New Array");
-  }, [myArray])
 
 
   return (
