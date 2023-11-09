a0a8b5a1fa9f9c9ae2b7d42c663595d795b5e5e6
yusufroqib <yusufroqib@gmail.com>
2023-11-09 11:52:10 +0100

    update

--- react-timer/src/App.jsx
+++ react-timer/src/App.jsx
@@ -20,7 +20,13 @@ const App = () => {
         <button>Stop</button>
         <button>Start</button>
       </section>
+
       <button>Reset</button>
+
+      <br />
+      <br />
+
+      <p>Seconds: </p>
 
     </main>
   );
