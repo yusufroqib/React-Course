2b8753806d0047db7be04746469e81164cffa145
yusufroqib <yusufroqib@gmail.com>
2023-11-09 10:50:03 +0100

    updates

--- react-use-memo/src/App.jsx
+++ react-use-memo/src/App.jsx
@@ -17,9 +17,16 @@ const App = () => {
 
   const fibNumber = useMemo(() => fib(userInput), [userInput, fib])
 
+  const myArray = useMemo(() => getArray(), [])
+
+  useEffect(() => {
+    console.log("New Array");
+  }, [myArray])
+
 
   return (
     <main className='App'>
+      <label >Fibonaci Sequence:</label>
       <input type="text" 
         placeholder='Input' 
         value={userInput} 
