56d59551bd37354a9c6c9059075392aaae51a1bd
yusufroqib <yusufroqib@gmail.com>
2023-11-09 11:45:25 +0100

    update

--- react-timer/src/App.jsx
+++ react-timer/src/App.jsx
@@ -1,36 +1,20 @@
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect } from "react";
 
 const App = () => {
-  const [userInput, setUserInput] = useState("")
-  const [randomInput, setRandomInput] = useState("")
-
-
-
+  const [randomInput, setRandomInput] = useState("");
 
   return (
-    <main className='App'>
-      <label >Fibonaci Sequence:</label>
-      <input type="text" 
-        placeholder='Position' 
-        value={userInput} 
-        onChange={(e) => setUserInput(e.target.value)} 
-      />
-      <p>Number: {fibNumber || "--"}</p>
-
-      <br />
-      <br />
-
-      <label >Random Input:</label>
-      <input type="text" 
+    <main className="App">
+      <label>Random Input:</label>
+      <input
+        type="text"
         value={randomInput}
-        placeholder='Type anything'
+        placeholder="Type anything"
         onChange={(e) => setRandomInput(e.target.value)}
       />
       <p>Result: {randomInput}</p>
-
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
