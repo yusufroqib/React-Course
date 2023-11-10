c032577ffc6f039f8d64768196a06c310e3a470a
yusufroqib <yusufroqib@gmail.com>
2023-11-10 10:12:57 +0100

    update

--- form-validation/src/Register.jsx
+++ form-validation/src/Register.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useEffect, useState, useRef } from "react"
+import {faCheck, faTimes, faInfocircle} from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import axios from "./api/axios"
+
+const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+const REGISTER_URL = "./Register.jsx"
 
 const Register = () => {
   return (
