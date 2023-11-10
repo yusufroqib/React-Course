b1b3d59ad77d3453e59a2518a8e03d1caafc49a1
yusufroqib <yusufroqib@gmail.com>
2023-11-10 10:33:58 +0100

    update

--- form-validation/src/Register.jsx
+++ form-validation/src/Register.jsx
@@ -4,10 +4,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import axios from "./api/axios"
 
 const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
-const REGISTER_URL = "./Register.jsx"
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+const REGISTER_URL = "/register"
 
 const Register = () => {
+    const userRef = useRef()
+    const errRef = useRef()
+
+    const [user, setUser] = useState("")
+    const [validName, setValidName] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false)
+
+    const [matchPwd, setMatchPwd] = useState('')
+    const [validMatch, setValidMatch] = useState(false)
+    const [matchFocus, setMatchFocus] = useState(false)
+
+    const [errMsg, setErrMsg] = useState("")
+    const [success, setSuccess] = useState(false)
+  
   return (
     <div>Register</div>
   )
