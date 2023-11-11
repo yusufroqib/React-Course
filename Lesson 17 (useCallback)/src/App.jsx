f1206e083c8c364eaa61a8165991825dfe0b7b51
yusufroqib <yusufroqib@gmail.com>
2023-11-11 13:00:26 +0100

    update

--- Lesson 17 (useCallback)/src/App.jsx
+++ Lesson 17 (useCallback)/src/App.jsx
@@ -6,14 +6,21 @@ const App = () => {
   const [num1] = useState(4)
   const [num2] = useState(5)
 
+
   const sum = useCallback(() => num1 + num2, [num1, num2])
-
-  const buildArray = useCallback(() => [num1, num2], [num1, num2])
-
   useEffect(() => {
     console.log(`New sum. Value: ${buildArray()}`);
     setResult(buildArray())
   }, [buildArray])
+
+  // const sum = useCallback(() => num1 + num2, [num1, num2])
+
+  // const buildArray = useCallback(() => [num1, num2], [num1, num2])
+
+  // useEffect(() => {
+  //   console.log(`New sum. Value: ${buildArray()}`);
+  //   setResult(buildArray())
+  // }, [buildArray])
 
 
   return (
