bbe94de3d64694b8ddc54db7e803a34662a8da89
yusufroqib <yusufroqib@gmail.com>
2023-11-13 15:49:57 +0100

    update

--- Lesson 22 (form-login-authentication)/src/Login.jsx
+++ Lesson 22 (form-login-authentication)/src/Login.jsx
@@ -1,69 +1,82 @@
-import React, { useEffect, useRef, useState } from 'react'
-
+import React, { useEffect, useRef, useState } from "react";
 
 function Login() {
-    const userRef = useRef()
-    const errRef = useRef()
+   const userRef = useRef();
+   const errRef = useRef();
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [errMsg, setErrMsg] = useState('')
-    const [success, setSuccess] = useState(false)
+   const [username, setUsername] = useState("");
+   const [password, setPassword] = useState("");
+   const [errMsg, setErrMsg] = useState("");
+   const [success, setSuccess] = useState(false);
 
-    useEffect(() => {
-      userRef.current.focus()
-    
-    }, [])
+   useEffect(() => {
+      userRef.current.focus();
+   }, []);
 
-    useEffect(() => {
-        setErrMsg('')
-    }, [username, password])
-    
+   useEffect(() => {
+      setErrMsg("");
+   }, [username, password]);
 
-  return (
-    <>
-        <section>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
-                {errMsg}
-            </p>
-            <h1>Sign In</h1>
-            <form >
-                <label>
-                    Username:
-                </label>
-                <input
-                        type="text"
-                        id='username'
-                        ref={userRef}
-                        autoComplete='off'
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                />
-                
-                <label>
-                    Password:
-                </label>
-                <input
-                    type="password"
-                    id='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button >
-                    Sign In
-                </button>
-            </form>
-            <p>
-                Need an Acount? <br />
-                <span>
-                <a href="#">Sign Up</a>
-                </span>
-            </p>
-        </section>
-    </>
-  )
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      if (username === "" || password === "") {
+         setErrMsg("Username and password are required");
+         return;
+      }
+      setSuccess(true);
+      setUsername("");
+      setPassword("");
+   };
+
+   return (
+      <>
+         {success ? (
+            <section>
+               <h1>Login Successful</h1>
+               <a href="/">Home</a>
+            </section>
+         ) : (
+            <section>
+               <p
+                  ref={errRef}
+                  className={errMsg ? "errmsg" : "offscreen"}
+                  aria-live="assertive"
+               >
+                  {errMsg}
+               </p>
+               <h1>Sign In</h1>
+               <form onSubmit={handleSubmit}>
+                  <label>Username:</label>
+                  <input
+                     type="text"
+                     id="username"
+                     ref={userRef}
+                     autoComplete="off"
+                     value={username}
+                     onChange={(e) => setUsername(e.target.value)}
+                     required
+                  />
+
+                  <label>Password:</label>
+                  <input
+                     type="password"
+                     id="password"
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                     required
+                  />
+                  <button>Sign In</button>
+               </form>
+               <p>
+                  Need an Acount? <br />
+                  <span>
+                     <a href="#">Sign Up</a>
+                  </span>
+               </p>
+            </section>
+         )}
+      </>
+   );
 }
 
-export default Login
+export default Login;
