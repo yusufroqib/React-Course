37b4ba9190654d797b945e22097b2948720c0c3f
yusufroqib <yusufroqib@gmail.com>
2023-11-14 08:50:36 +0100

    update

---
+++ Lesson 22 (form-login-authentication)/src/context/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import {createContext, useContext, useState } from "react";
+
+const AuthContext = createContext({})
+
+export const AuthProvider = ({children}) => {
+    const [auth, setAuth] = useState({})
+
+    return (
+        <AuthContext.Provider value={{auth, setAuth}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext
