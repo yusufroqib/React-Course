050ca6e94464a046b3095fe999be7d02b0036dbb
yusufroqib <yusufroqib@gmail.com>
2023-11-15 11:03:00 +0100

    update

---
+++ Lesson 22 (form-login-authentication)/src/components/Users.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "../api/axios";
+
+const Users = () => {
+   const [users, setUsers] = useState([]);
+
+   useEffect(() => {
+      let isMounted = true;
+      const controller = new AbortController();
+
+      const getUsers = async () => {
+         try {
+            const response = await axios.get("/users", {
+               signal: controller.signal,
+            });
+            console.log(response.data);
+            isMounted && setUsers(response.data);
+         } catch (err) {
+            console.log(err);
+         }
+      };
+      getUsers();
+      return () => {
+         isMounted = false;
+         controller.abort();
+      };
+   }, []);
+
+   return (
+      <article>
+         <h2>Users List</h2>
+         {users?.length ? (
+            <ul>
+               {users.map((user, i) => (
+                  <li key={i}>{user?.username}</li>
+               ))}
+            </ul>
+         ) : (
+            <p>No user to display</p>
+         )}
+      </article>
+   );
+};
+
+export default Users;
