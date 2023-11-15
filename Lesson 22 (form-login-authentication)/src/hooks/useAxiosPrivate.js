157967b062f8221b2606741b19f697a6168bdbbc
yusufroqib <yusufroqib@gmail.com>
2023-11-15 15:15:25 +0100

    update

--- Lesson 22 (form-login-authentication)/src/hooks/useAxiosPrivate.js
+++ Lesson 22 (form-login-authentication)/src/hooks/useAxiosPrivate.js
@@ -15,6 +15,23 @@ const useAxiosPrivate = () => {
                 return config
             }, (error) => Promise.reject(error)
         )
+        const responseIntercept = axiosPrivate.interceptors.response.use(
+            response => response, 
+            async (error) => {
+                const prevRequest = error?.config
+                if (error?.response?.status === 403 && !prevRequest?.sent) {
+                    prevRequest.sent = true
+                    const newAccessToken = await refresh()
+                    prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`
+                }
+                return Promise.reject(error)
+            }
+        )
+
+        return () => {
+            axiosPrivate.interceptors.request.eject(requestIntercept)
+            axiosPrivate.interceptors.response.eject(responseIntercept)
+        }
     }, [auth, refresh])
   return axiosPrivate
 }
