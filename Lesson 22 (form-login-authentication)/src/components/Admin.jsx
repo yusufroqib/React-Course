440734ca35579e2363a34afcdb83ed519f841bf9
yusufroqib <yusufroqib@gmail.com>
2023-11-15 11:21:27 +0100

    update

--- Lesson 22 (form-login-authentication)/src/components/Admin.jsx
+++ Lesson 22 (form-login-authentication)/src/components/Admin.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Users from './Users'
 
 const Admin = () => {
   return (
     <section>
         <h1>Admin Page</h1>
         <br />
-        <p>You must have been assigned an Admin Role</p>
+        {/* <p>You must have been assigned an Admin Role</p> */}
+        <Users />
 
         <div className="flexGrow">
             <Link to='/'>Home</Link>
