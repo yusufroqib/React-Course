da495d9ac16acb8c83396b81279714e5b8769c65
yusufroqib <yusufroqib@gmail.com>
2023-11-15 11:32:25 +0100

    update

--- Lesson 22 (form-login-authentication)/src/components/RequireAuth.jsx
+++ Lesson 22 (form-login-authentication)/src/components/RequireAuth.jsx
@@ -6,7 +6,7 @@ const RequireAuth = ({allowedRoles}) => {
     const { auth } = useAuth()
     const location = useLocation()
   return (
-    auth?.roles?.find(role => allowedRoles.includes(role)) ?
+    auth?.roles?.find(role => allowedRoles?.includes(role)) ?
     <Outlet />
     :
     auth?.user?
