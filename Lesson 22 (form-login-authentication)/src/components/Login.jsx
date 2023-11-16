6afd41fe67b13fce8fa7c6e84b1468a715546a26
yusufroqib <yusufroqib@gmail.com>
2023-11-16 15:58:14 +0100

    update

--- Lesson 22 (form-login-authentication)/src/components/Login.jsx
+++ Lesson 22 (form-login-authentication)/src/components/Login.jsx
@@ -1,16 +1,15 @@
-import { useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "../api/axios";
 import useAuth from "../hooks/useAuth";
 import { useNavigate, Link, useLocation } from "react-router-dom";
 
-
 function Login() {
-   const LOGIN_URL = '/auth'
-   const {setAuth, persist, setPersist} = useAuth()
+   const LOGIN_URL = "/auth";
+   const { setAuth, persist, setPersist } = useAuth();
 
-   const navigate = useNavigate()
-   const location = useLocation()
-   const from = location.state?.from?.pathname || '/'
+   const navigate = useNavigate();
+   const location = useLocation();
+   const from = location.state?.from?.pathname || "/";
 
    const userRef = useRef();
    const errRef = useRef();
@@ -30,97 +29,97 @@ function Login() {
    const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-         const response = await axios.post(LOGIN_URL, JSON.stringify({user, pwd}), {
-            headers: {'Content-Type': 'application/json'},
-            withCredentials: true
-         })
-      console.log(JSON.stringify(response?.data));
-      const accessToken = response?.data?.accessToken
-      const roles = response?.data?.roles
+         const response = await axios.post(
+            LOGIN_URL,
+            JSON.stringify({ user, pwd }),
+            {
+               headers: { "Content-Type": "application/json" },
+               withCredentials: true,
+            }
+         );
+         console.log(JSON.stringify(response?.data));
+         const accessToken = response?.data?.accessToken;
+         const roles = response?.data?.roles;
 
-      setAuth({user, pwd, roles, accessToken})
+         setAuth({ user, pwd, roles, accessToken });
 
-      setUser("");
-      setPwd("");
-      navigate(from, {replace: true})
+         setUser("");
+         setPwd("");
+         navigate(from, { replace: true });
       } catch (error) {
          if (!error.response) {
             setErrMsg("No Server Response");
-         } else if (error.response?.status === 400){
-            setErrMsg('Missing Username or Password')
-         } else if (error.response?.status === 401){
-            setErrMsg('Unauthorized')
+         } else if (error.response?.status === 400) {
+            setErrMsg("Missing Username or Password");
+         } else if (error.response?.status === 401) {
+            setErrMsg("Unauthorized");
          } else {
-            setErrMsg('Login Failed')
+            setErrMsg("Login Failed");
          }
-         errRef.current.focus()
+         errRef.current.focus();
       }
       // if (user === "" || pwd === "") {
       //    setErrMsg("User and pwd are required");
       //    return;
       // }
-    
    };
 
    const togglePersist = () => {
-      setPersist(prev => !prev)
-   }
+      setPersist((prev) => !prev);
+   };
 
    useEffect(() => {
-      localStorage.setItem('persist', persist)
-   }, [persist])
-   
+      localStorage.setItem("persist", persist);
+   }, [persist]);
 
    return (
-      
-            <section>
-               <p
-                  ref={errRef}
-                  className={errMsg ? "errmsg" : "offscreen"}
-                  aria-live="assertive"
-               >
-                  {errMsg}
-               </p>
-               <h1>Sign In</h1>
-               <form onSubmit={handleSubmit}>
-                  <label>Username:</label>
-                  <input
-                     type="text"
-                     id="username"
-                     ref={userRef}
-                     autoComplete="off"
-                     value={user}
-                     onChange={(e) => setUser(e.target.value)}
-                     required
-                  />
+      <section>
+         <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+         >
+            {errMsg}
+         </p>
+         <h1>Sign In</h1>
+         <form onSubmit={handleSubmit}>
+            <label>Username:</label>
+            <input
+               type="text"
+               id="username"
+               ref={userRef}
+               autoComplete="off"
+               value={user}
+               onChange={(e) => setUser(e.target.value)}
+               required
+            />
 
-                  <label>Password:</label>
-                  <input
-                     type="password"
-                     id="password"
-                     value={pwd}
-                     onChange={(e) => setPwd(e.target.value)}
-                     required
-                  />
-                  <button>Sign In</button>
-                  <div className="persistCheck">
-                     <input
-                        type="checkbox"
-                        id="persist"
-                        checked={persist}
-                        onChange={togglePersist}
-                     />
-                     <label htmlFor="persist">Trust this device</label>
-                  </div>
-               </form>
-               <p>
-                  Need an Acount? <br />
-                  <span className="line">
-                     <Link to='/register'>Sign Up</Link>
-                  </span>
-               </p>
-            </section>
-        
+            <label>Password:</label>
+            <input
+               type="password"
+               id="password"
+               value={pwd}
+               onChange={(e) => setPwd(e.target.value)}
+               required
+            />
+            <button>Sign In</button>
+            <div className="persistCheck">
+               <input
+                  type="checkbox"
+                  id="persist"
+                  checked={persist}
+                  onChange={togglePersist}
+               />
+               <label htmlFor="persist">Trust this device</label>
+            </div>
+         </form>
+         <p>
+            Need an Acount? <br />
+            <span className="line">
+               <Link to="/register">Sign Up</Link>
+            </span>
+         </p>
+      </section>
    );
 }
 
