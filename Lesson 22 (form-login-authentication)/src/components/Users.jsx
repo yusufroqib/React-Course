3405e0381ebf8f0a9bdfcf4a3876131de2701132
yusufroqib <yusufroqib@gmail.com>
2023-11-16 09:40:15 +0100

    update

--- Lesson 22 (form-login-authentication)/src/components/Users.jsx
+++ Lesson 22 (form-login-authentication)/src/components/Users.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 const Users = () => {
    const [users, setUsers] = useState([]);
    const axiosPrivate = useAxiosPrivate()
+   const navigate = useNavigate()
+   const location = useLocation()
 
    useEffect(() => {
       let isMounted = true;
