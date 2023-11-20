96bb8ed6e953ca9a82a9fed5fbc279ee0a248116
yusufroqib <yusufroqib@gmail.com>
2023-11-20 13:00:56 +0100

    update

--- react-async-await-request/src/hooks/useAxios.js
+++ react-async-await-request/src/hooks/useAxios.js
@@ -11,4 +11,30 @@ const useAxios = (configObj) => {
     const [response, setResponse] = useState([])
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const controller = new AbortController()
+        const fetchData = async () => {
+            try {
+                const res = await axiosInstance[method.toLowerCase()](url, {
+                    ...requestConfig,
+                    signal: controller.signal
+                })
+                console.log(res);
+                setResponse(res.data)
+            } catch (error) {
+                console.log(error);
+                setError(error.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData()
+        return () => controller.abort()
+    }, [])
+
+    return [response, loading, error]
+    
 }
+
+export default useAxios
