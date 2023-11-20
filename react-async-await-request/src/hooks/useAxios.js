81f1ab76261be6d1e5a6b6042db9e64dc83e4d7f
yusufroqib <yusufroqib@gmail.com>
2023-11-20 15:39:11 +0100

    update

--- react-async-await-request/src/hooks/useAxios.js
+++ react-async-await-request/src/hooks/useAxios.js
@@ -11,6 +11,9 @@ const useAxios = (configObj) => {
     const [response, setResponse] = useState([])
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(true)
+    const [reload, setReload] = useState(0)
+
+    const refetch = () => setReload(prev => prev + 1)
 
     useEffect(() => {
         let isMounted = true;
