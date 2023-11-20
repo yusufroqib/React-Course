83772d765f45e78ed2199ba7e384005682820e78
yusufroqib <yusufroqib@gmail.com>
2023-11-20 10:56:15 +0100

    update

--- Lesson 22 (form-login-authentication)/src/hooks/useInput.js
+++ Lesson 22 (form-login-authentication)/src/hooks/useInput.js
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import useLocalStorage from './useLocalStorage'
-
-
 
 const useInput = (key, initValue) => {
     const [value, setValue] = useLocalStorage(key, initValue);
