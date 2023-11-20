b2c3268bd1e5c7ed8f35ffc41425cf81e9a4062e
yusufroqib <yusufroqib@gmail.com>
2023-11-20 12:51:31 +0100

    update

--- react-async-await-request/src/hooks/useAxios.js
+++ react-async-await-request/src/hooks/useAxios.js
@@ -1,5 +1,14 @@
 import { useState, useEffect } from "react";
 
-const useAxios = () => {
-    
+const useAxios = (configObj) => {
+    const {
+            axiosInstance,
+            method,
+            url,
+            requestConfig = {}
+    } = configObj
+
+    const [response, setResponse] = useState([])
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(true)
 }
