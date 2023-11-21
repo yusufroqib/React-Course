22c4f7614b4c2f95c120bf1979b012be9376879d
yusufroqib <yusufroqib@gmail.com>
2023-11-21 09:46:53 +0100

    update

--- react-async-await-request/src/components/Posts.jsx
+++ react-async-await-request/src/components/Posts.jsx
@@ -1,1 +1,55 @@
-import axios from '../apis/jsonPH'
+import { useEffect } from "react";
+import axios from "../apis/jsonPH";
+import useAxiosFunction from "../hooks/useAxiosFunction";
+
+const Posts = () => {
+   const [posts, loading, error, axiosFetch] = useAxiosFunction;
+   const getData = () => {
+      axiosFetch({
+         axiosInstance: axios,
+         url: "/posts",
+         method: "GET",
+      });
+   };
+
+   useEffect(() => {
+      getData();
+   }, []);
+
+   const handleSubmit = () => {
+      axiosFetch({
+         axiosInstance: axios,
+         url: "/posts",
+         method: "POST",
+         requestConfig: {
+            data: {
+               userId: 10,
+               title: "Supreme",
+               body: "Holla at your boy...",
+            },
+         },
+      });
+   };
+
+   return (
+      <article>
+         <h2>Posts</h2>
+
+         {loading && <p>Loading...</p>}
+         {!loading && error && <p className="errMsg">{error}</p>}
+         {!loading && !error && posts?.length && (
+            <ul>
+                {
+                    posts.map((post, i) => (
+                        <li key={i}>{post.id}. {post.title}</li>
+                    ))
+                }
+            </ul>
+         )}
+         {!loading && !error && !posts?.length && <p>No Post to Display! 😞</p>}
+
+      </article>
+   );
+};
+
+export default Posts;
