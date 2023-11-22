b3f649faa6aa41258cd4e979dec39c04bb2dbac8
yusufroqib <yusufroqib@gmail.com>
2023-11-22 15:29:52 +0100

    update

--- react-infinite-scroll/src/App.jsx
+++ react-infinite-scroll/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import Example1 from './Example1'
+import Example2 from './Example2'
 
 
 function App() {
 
   return (
     <div className='App'>
-      <Example1/>
+      <Example2/>
     </div>
   )
 }
--- react-infinite-scroll/src/Example2.jsx
+++ react-infinite-scroll/src/Example2.jsx
@@ -6,13 +6,18 @@ import { useInfiniteQuery } from "react-query";
 import { getPostsPage } from "./api/axios";
 
 const Example2 = () => {
-    const {fetchNextPage, hasNextPage, isFetchingNextPage, date, status, error} = useInfiniteQuery('/posts', ({pageParam = 1}) => getPostsPage())
+    const {fetchNextPage, hasNextPage, isFetchingNextPage, data, status, error} = useInfiniteQuery('/posts', 
+        ({pageParam = 1}) => getPostsPage(pageParam), 
+        {getNextPageParam: (lastPage, allPages) => {
+            return lastPage.length ? allPages.length + 1 : undefined
+        }}
+    )
    
     const intObserver = useRef()
    
     const lastPostRef = useCallback((post) => {
-      if (loading) return;
-
+      if (isFetchingNextPage) return;
+ 
       if (intObserver.current) intObserver.current.disconnect();
 
       const confirmLoad = () => {
@@ -23,7 +28,7 @@ const Example2 = () => {
                {
                   label: "Yes",
                   onClick: () => {
-                     setPageNum(prev => prev + 1);
+                     fetchNextPage();
                   }
                },
                {label: "No"}
@@ -34,38 +39,43 @@ const Example2 = () => {
      intObserver.current = new IntersectionObserver((post) => {
         if (post[0].isIntersecting && hasNextPage) {
            console.log("we are near the last post");
-            confirmLoad();}
+            confirmLoad()
+        }
      });
-
       if (post) intObserver.current.observe(post)
-   }, [loading, hasNextPage]);
+   }, [isFetchingNextPage, fetchNextPage, hasNextPage]);
 
-   const content = results.map((post, i) => {
-      if(results.length === i + 1) {
-         return <Post ref={lastPostRef} key={post.id} post={post} />
-      }
-      
-      return <Post key={post.id} post={post} />
-   })
+   if (status === 'error') return <p className="center">Error: {error.message}</p>;
+
+   const content = data?.pages.map(pg => {
+      return pg.map((post, i) => {
+         if (pg.length === i + 1) {
+            return <Post ref={lastPostRef} key={post.id} post={post} />;
+         }
+         return <Post key={post.id} post={post} />;
+      });
+   });
    
-   if (isError) return <p className="center">Error: {error.message}</p>
-  return (
-     <>
-        <h1 id="top">
-           &infin; Infinite Query &amp; Scroll
-           <br />
-           &infin; Ex. 1 - React Only
-        </h1>
 
-        {content}
+   
 
-        <p className="center">Loading more posts...</p>
+   return (
+      <>
+         <h1 id="top">
+            &infin; Infinite Query &amp; Scroll
+            <br />
+            &infin; Ex. 2 - React Query
+         </h1>
 
-        <p className="center">
-           <a href="#top">Back to Top</a>
-        </p>
-     </>
-  );
+         {content}
+
+         <p className="center">Loading more posts...</p>
+
+         <p className="center">
+            <a href="#top">Back to Top</a>
+         </p>
+      </>
+   );
 }
 
 export default Example2
