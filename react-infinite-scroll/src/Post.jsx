a0b860671213d761af2246923409380e280659fa
yusufroqib <yusufroqib@gmail.com>
2023-11-22 11:53:32 +0100

    update

--- react-infinite-scroll/src/Post.jsx
+++ react-infinite-scroll/src/Post.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Post = React.forwardRef(() => {
-  return (
-    <div>Post</div>
-  )
+const Post = React.forwardRef(({post}, ref) => {
+    const postBody = (
+        <>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <p>Post ID: {post.id}</p>
+        </>
+    )
+
+    const content = ref ? (
+        <article ref={ref}>{postBody}</article>
+    ) : (
+        <article>{postBody}</article>
+    )
+
+
+  return content
 })
 
 export default Post
