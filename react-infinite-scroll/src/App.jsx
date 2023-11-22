580e4c47c516382c7a7872f9ac552f7c71aea77b
yusufroqib <yusufroqib@gmail.com>
2023-11-22 10:08:35 +0100

    update

---
+++ react-infinite-scroll/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Example1 from './Example1'
+
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Example1/>
+    </div>
+  )
+}
+
+export default App
---
+++ react-infinite-scroll/src/Example1.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Example1 = () => {
+  return (
+     <>
+        <h1 id="top">
+           &infin; Infinite Query &amp; Scroll
+           <br />
+           &infin; Ex. 1 - React Only
+        </h1>
+
+        <p className="center">Loading more posts...</p>
+
+        <p className="center">
+           <a href="#top">Back to Top</a>
+        </p>
+     </>
+  );
+}
+
+export default Example1
