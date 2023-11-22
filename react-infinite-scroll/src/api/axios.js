9b741e8cbf5141530a808687b18f1ffed7a7cbfa
yusufroqib <yusufroqib@gmail.com>
2023-11-22 10:15:32 +0100

    update

---
+++ react-infinite-scroll/src/api/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com/"
+})
+
+export const getPostsPage = async (pageParam = 1, options = {}) => {
+    const response = await api.get
+}
