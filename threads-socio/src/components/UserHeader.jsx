20bb740cee648c8c6518dddaa992bc9ea2251e28
yusufroqib <yusufroqib@gmail.com>
2023-11-23 14:42:40 +0100

    update

--- threads-socio/src/components/UserHeader.jsx
+++ threads-socio/src/components/UserHeader.jsx
@@ -5,7 +5,7 @@ const UserHeader = () => {
       <VStack gap={4} alignItems={"start"}>
          <Flex justifyContent={"space-between"} w={"full"}>
             <Box>
-                <Text>Aliu </Text>
+                <Text>Aliu Musa</Text>
             </Box>
          </Flex>
       </VStack>
