2213c36efa7e79d5fdf4180d7f162fe7c9f6ba2b
yusufroqib <yusufroqib@gmail.com>
2023-11-23 14:31:31 +0100

    update

--- threads-socio/src/components/UserHeader.jsx
+++ threads-socio/src/components/UserHeader.jsx
@@ -1,8 +1,15 @@
+import { Box, Flex, Text, VStack } from "@chakra-ui/layout";
 
 const UserHeader = () => {
-  return (
-    <div>UserHeader</div>
-  )
-}
+   return (
+      <VStack gap={4} alignItems={"start"}>
+         <Flex justifyContent={"space-between"} w={"full"}>
+            <Box>
+                <Text>Aliu </Text>
+            </Box>
+         </Flex>
+      </VStack>
+   );
+};
 
-export default UserHeader
+export default UserHeader;
