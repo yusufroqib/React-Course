2cd8b42717811cd7db833753253b62f2b9423c65
yusufroqib <yusufroqib@gmail.com>
2023-11-27 14:56:55 +0100

    update

--- threads-socio/src/pages/PostPage.jsx
+++ threads-socio/src/pages/PostPage.jsx
@@ -3,23 +3,30 @@ import React from "react";
 import { BsThreeDots } from "react-icons/bs";
 
 const PostPage = () => {
-   return (
-      <>
-         <Flex>
-            <Flex w={"full"} gap={3} alignItems={"center"}>
-               <Avatar src="/aliumusa.jpeg" name="Aliu Musa" size={"md"} />
-               <Flex alignItems={"center"}>
-                  <Text fontSize={"sm"} >Aliu Musa</Text>
-                  <Image src="/verified.png" h={4} w={4} ml={2} />
-               </Flex>
-               <Flex alignItems={"center"}>
-                  <Text fontSize={"sm"} >2day</Text>
-                  <BsThreeDots />
-               </Flex>
-            </Flex>
-         </Flex>
-      </>
-   );
+  return (
+    <>
+      <Flex>
+        <Flex
+          w={"full"}
+          gap={3}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Avatar src="/aliumusa.jpeg" name="Aliu Musa" size={"md"} />
+          <Flex alignItems={"center"}>
+            <Text fontSize={"sm"}>Aliu Musa</Text>
+            <Image src="/verified.png" h={4} w={4} ml={2} />
+          </Flex>
+          <Flex alignItems={"center"} gap={4}>
+            <Text fontSize={"sm"} w={36} color={"gray.light"}>
+              2day
+            </Text>
+            <BsThreeDots />
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
 };
 
 export default PostPage;
