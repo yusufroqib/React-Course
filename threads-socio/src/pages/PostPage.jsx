62dee5b31ef6182d17ddc29cb4dee60c19b7033e
yusufroqib <yusufroqib@gmail.com>
2023-11-27 14:49:51 +0100

    update

--- threads-socio/src/pages/PostPage.jsx
+++ threads-socio/src/pages/PostPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Avatar, Flex, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { BsThreeDots } from "react-icons/bs";
 
 const PostPage = () => {
-  return (
-    <div>PostPage</div>
-  )
-}
+   return (
+      <>
+         <Flex>
+            <Flex w={"full"} gap={3} alignItems={"center"}>
+               <Avatar src="/aliumusa.jpeg" name="Aliu Musa" size={"md"} />
+               <Flex alignItems={"center"}>
+                  <Text fontSize={"sm"} >Aliu Musa</Text>
+                  <Image src="/verified.png" h={4} w={4} ml={2} />
+               </Flex>
+               <Flex alignItems={"center"}>
+                  <Text fontSize={"sm"} >2day</Text>
+                  <BsThreeDots />
+               </Flex>
+            </Flex>
+         </Flex>
+      </>
+   );
+};
 
-export default PostPage
+export default PostPage;
