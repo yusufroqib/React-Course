e277095655da36a13743cd3d00878b6fbcf139a8
yusufroqib <yusufroqib@gmail.com>
2023-11-27 16:02:02 +0100

    update

--- threads-socio/src/components/Comments.jsx
+++ threads-socio/src/components/Comments.jsx
@@ -1,5 +1,6 @@
-import { Avatar, Flex } from "@chakra-ui/react";
+import { Avatar, Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import { BsThreeDots } from "react-icons/bs";
 
 const Comments = ({ comment, createdAt, likes, username, userAvater }) => {
   return (
@@ -10,15 +11,20 @@ const Comments = ({ comment, createdAt, likes, username, userAvater }) => {
           size={"sm"}
           name="Ryan Florence"
         />
-        <Flex
-          flexDir={"column"}
-          gap={1}
-          w={"full"}
-          border={"2px solid white"}
-        >
-            <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
-                <Text>JohnDoe</Text>
+        <Flex flexDir={"column"} gap={1} w={"full"} border={"2px solid white"}>
+          <Flex
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            w={"full"}
+          >
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              JohnDoe
+            </Text>
+            <Flex gap={2} alignItems={"center"}>
+                <Text>2 min ago</Text>
+                <BsThreeDots />
             </Flex>
+          </Flex>
         </Flex>
       </Flex>
     </>
