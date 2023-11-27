86d6b0e1859d73b59e9248bb80e73b8db6610bde
yusufroqib <yusufroqib@gmail.com>
2023-11-27 15:54:34 +0100

    update

--- threads-socio/src/components/Comments.jsx
+++ threads-socio/src/components/Comments.jsx
@@ -1,14 +1,21 @@
-import { Avatar, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Avatar, Flex } from "@chakra-ui/react";
+import React from "react";
 
-const Comments = ({comment, createdAt, likes, username, userAvater}) => {
+const Comments = ({ comment, createdAt, likes, username, userAvater }) => {
   return (
     <>
-        <Flex gap={4} my={2} py={2} w={"full"}>
-            <Avatar src='https://bit.ly/ryan-florence' size={"sm"} name='Ryan Florence'/>
+      <Flex gap={4} my={2} py={2} w={"full"}>
+        <Avatar
+          src="https://bit.ly/ryan-florence"
+          size={"sm"}
+          name="Ryan Florence"
+        />
+        <Flex>
+            
         </Flex>
+      </Flex>
     </>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
