8ac6f4c9b30d340b8b384e0ff7e89fcaa2abfb8c
yusufroqib <yusufroqib@gmail.com>
2023-11-28 12:32:37 +0100

    update

--- threads-social-clone/threads-backend/models/postModel.js
+++ threads-social-clone/threads-backend/models/postModel.js
@@ -1,49 +1,49 @@
 import mongoose from "mongoose";
 
-const postSchema =  mongoose.Schema(
-    {
-        postedBy: {
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: "User",
-            required: true
+const postSchema = mongoose.Schema(
+  {
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      maxLength: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+      default: [],
+      required: true,
+    },
+    replies: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
         },
         text: {
-            type: String,
-            maxLength: 500
+          type: String,
+          required: true,
         },
-        img: {
-            type: String,
+        userProfilePic: {
+          type: String,
         },
-        likes: {
-            type: [mongoose.Schema.Types.ObjectId],
-            ref: "User",
-            default: [],
-            required: true
+        username: {
+          type: String,
         },
-        replies: [
-            {
-                userId: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "User",
-                    required: true
-                },
-                text: {
-                    type: String,
-                    required: true
-                },
-                userProfilePic: {
-                    type: String,
-                },
-                username: {
-                    type: String
-                }
-
-
-            }
-        ]
-       
-    }
-)
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Post = mongoose.model("Post", postSchema);
-export  default Post;
+export default Post;
