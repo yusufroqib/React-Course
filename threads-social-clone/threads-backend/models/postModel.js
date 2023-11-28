90dea0a266a6894265e0249cd4ffef9b4be35dcc
yusufroqib <yusufroqib@gmail.com>
2023-11-28 12:28:49 +0100

    update

---
+++ threads-social-clone/threads-backend/models/postModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const postSchema =  mongoose.Schema(
+    {
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "User",
+            required: true
+        },
+        text: {
+            type: String,
+            maxLength: 500
+        },
+        img: {
+            type: String,
+        },
+        likes: {
+            type: [mongoose.Schema.Types.ObjectId],
+            ref: "User",
+            default: [],
+            required: true
+        },
+        replies: [
+            {
+                userId: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "User",
+                    required: true
+                },
+                text: {
+                    type: String,
+                    required: true
+                },
+                userProfilePic: {
+                    type: String,
+                },
+                username: {
+                    type: String
+                }
+
+
+            }
+        ]
+       
+    }
+)
+
+const Post = mongoose.model("Post", postSchema);
+export  default Post;
