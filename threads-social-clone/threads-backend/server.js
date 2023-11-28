e81a934cc71802d0ca7a068aaab4c600949d01e8
yusufroqib <yusufroqib@gmail.com>
2023-11-28 11:22:03 +0100

    update

---
+++ threads-social-clone/threads-backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from 'dotenv'
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import connectDB from "./db/connectDB";
+
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({limit: "50mb"})); //parse json data inside the req body
+app.use(express.urlencoded({extended: true})) //parse form data inside the req body
+app.use(cookieParser())
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome Home 🏡")
+})
+
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
