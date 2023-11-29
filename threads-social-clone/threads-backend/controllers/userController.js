50ba3c82fd2d2f4253f4c8d6bd97529bfe77c291
yusufroqib <yusufroqib@gmail.com>
2023-11-29 12:11:08 +0100

    update

--- threads-social-clone/threads-backend/controllers/userController.js
+++ threads-social-clone/threads-backend/controllers/userController.js
@@ -98,7 +98,7 @@ const followUnFollowUser = async (req, res) => {
 		const isFollowing = currentUser.following.includes(id)
 		if(!isFollowing) {
 			//Unfollow User
-			await User.findByIdAndUpdate(id, {$pull: {followers: req.user._id}})
+			await User.findByIdAndUpdate(id, {$pull: {followers: id}})
 		}
 	} catch (error) {
 		
