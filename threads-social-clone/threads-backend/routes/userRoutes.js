e5aea303b1ce655acd81e1783cd005f4dfc05b03
yusufroqib <yusufroqib@gmail.com>
2023-11-29 11:45:57 +0100

    update

--- threads-social-clone/threads-backend/routes/userRoutes.js
+++ threads-social-clone/threads-backend/routes/userRoutes.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const { signUpUser, loginUser, logoutUser } = require('../controllers/userController');
+const express = require("express");
+const {
+	signUpUser,
+	loginUser,
+	logoutUser,
+} = require("../controllers/userController");
+const protectRoute = require("../middleware/protectRoutes");
 
 const router = express.Router();
 
-router.post("/signup", signUpUser)
-router.post("/login", loginUser)
-router.post("/logout", logoutUser)
+router.post("/signup", signUpUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser); //toggle state(follow/unfollow)
 
 module.exports = router;
