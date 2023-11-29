28e2c728cee84018c2ce6e5ff120c637f5f5fd00
yusufroqib <yusufroqib@gmail.com>
2023-11-29 15:30:05 +0100

    update

--- threads-social-clone/threads-backend/controllers/userController.js
+++ threads-social-clone/threads-backend/controllers/userController.js
@@ -13,7 +13,10 @@ const getUserProfile = async (req, res) => {
 		let user;
 		//query is userId
 		if(mongoose.Types.ObjectId.isValid(query)){
-			user = await User.findOne({_id: query})
+			user = await User.findOne({_id: query}).select("-password").select("-updatedAt")
+		} else {
+			//query is username
+			user = await User.findOne({username: query}).select("-password").select("-updatedAt")
 		}
 	} catch (error) {}
 };
