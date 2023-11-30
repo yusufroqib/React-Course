44733b0418bda9ad0e30d55055f4be605e270d3d
yusufroqib <yusufroqib@gmail.com>
2023-11-30 11:40:09 +0100

    update

--- threads-social-clone/threads-backend/controllers/postControllers.js
+++ threads-social-clone/threads-backend/controllers/postControllers.js
@@ -5,30 +5,32 @@ const createPost = async (req, res) => {
 	try {
 		const { postedBy, text, img } = req.body;
 		if (!postedBy || !text) {
-			return res.status(400).json({ message: "postedBy and text fields are required." });
+			return res
+				.status(400)
+				.json({ message: "postedBy and text fields are required." });
 		}
 
-        const user = await User.findById(postedBy);
+		const user = await User.findById(postedBy);
 
-        if (!user) {
-            return res.status(404).json({ message: "User not found." });
-        
-        }
+		if (!user) {
+			return res.status(404).json({ message: "User not found." });
+		}
 
-        const maxLength = 500;
+		const maxLength = 500;
 
-        if (text.length > maxLength) {
-            return res.status(400).json({ message: `Text must be less than ${maxLength} characters` });
-            const newPost = new Post({postedBy, text, img})
-            await newPost.save();
-            res.status(201).json({message: "Post created successfully"});
-        
-        }
+		if (text.length > maxLength) {
+			return res
+				.status(400)
+				.json({ message: `Text must be less than ${maxLength} characters` });
+		}
 
+		const newPost = new Post({ postedBy, text, img });
+		await newPost.save();
+		res.status(201).json({ message: "Post created successfully", newPost });
 	} catch (error) {
-        res.status(500).json({ message: error.message }); //Internal server error
+		res.status(500).json({ message: error.message }); //Internal server error
 		console.log("Error in Create Post: ", error.message);
-    }
+	}
 };
 
 module.exports = {
