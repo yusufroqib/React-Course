7cd6612113dbf17e6fb40d33826796729c0ad5aa
yusufroqib <yusufroqib@gmail.com>
2023-11-30 10:03:53 +0100

    update

--- threads-social-clone/threads-backend/controllers/userController.js
+++ threads-social-clone/threads-backend/controllers/userController.js
@@ -157,6 +157,9 @@ const updateUser = async (req, res) => {
 		let user = await User.findById(userId);
 		if (!user) return res.status(400).json({ message: "User not found" });
 
+		if (req.params.id !== userId.toString())
+			return res.status(400).json({ message: "You cannot update other user's profile" })
+
 		if (password) {
 			const salt = await bcrypt.genSalt(10);
 			const hashedPassword = await bcrypt.hash(password, salt);
