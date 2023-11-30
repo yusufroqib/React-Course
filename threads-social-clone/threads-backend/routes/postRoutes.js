cdb6ba0f91da36149a3e3ad1fb421f3c1d9272bf
yusufroqib <yusufroqib@gmail.com>
2023-11-30 12:25:05 +0100

    update

--- threads-social-clone/threads-backend/routes/postRoutes.js
+++ threads-social-clone/threads-backend/routes/postRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const { createPost, getPost } = require("../controllers/postControllers")
+const { createPost, getPost, deletePost } = require("../controllers/postControllers")
 const protectRoute = require("../middleware/protectRoutes")
 
 const router = express.Router()
 
 router.get("/:id", getPost)
-
 router.post("/create", protectRoute, createPost)
+router.delete("/:id", deletePost)
 
 module.exports = router
