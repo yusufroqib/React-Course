e08b506389687171c41dbeb84f70bde14edec558
yusufroqib <yusufroqib@gmail.com>
2023-11-30 14:22:19 +0100

    update

--- threads-social-clone/threads-backend/controllers/postControllers.js
+++ threads-social-clone/threads-backend/controllers/postControllers.js
@@ -34,31 +34,32 @@ const createPost = async (req, res) => {
 };
 
 const getPost = async (req, res) => {
-    try {
+	try {
 		const post = await Post.findById(req.params.id);
 
-        if (!post) {
-            return res.status(404).json({ message: "Post not found" });
-        }
+		if (!post) {
+			return res.status(404).json({ message: "Post not found" });
+		}
 		res.status(200).json({ post });
 	} catch (error) {
 		res.status(500).json({ message: error.message }); //Internal server error
 		console.log("Error in Get Post: ", error.message);
 	}
-}
+};
 
 const deletePost = async (req, res) => {
-    try {
+	try {
 		const post = await Post.findById(req.params.id);
 
-        if (!post) {
-            return res.status(404).json({ message: "Post not found" });
-        }
+		if (!post) {
+			return res.status(404).json({ message: "Post not found" });
+		}
 
-        if (post.postedBy.toString() !== req.user._id.toString()) {
-            return res.status(401).json({ message: "Unauthorized to delete this post" }); //Unauthorized
-
-        }
+		if (post.postedBy.toString() !== req.user._id.toString()) {
+			return res
+				.status(401)
+				.json({ message: "Unauthorized to delete this post" }); //Unauthorized
+		}
 
 		await Post.findByIdAndDelete(req.params.id);
 		res.status(200).json({ message: "Post deleted successfully" });
@@ -66,16 +67,37 @@ const deletePost = async (req, res) => {
 		res.status(500).json({ message: error.message }); //Internal server error
 		console.log("Error in Delete Post: ", error.message);
 	}
-
-}
+};
 
 const likeUnlikePost = async (req, res) => {
-    
-}
+	try {
+		const { id: postId } = req.params;
+		const userId = req.user._id;
+		const post = await Post.findById(postId);
+
+		if (!post) {
+			return res.status(404).json({ message: "Post not found" });
+		}
+
+		const userLikedPost = post.likes.includes(userId);
+
+		if (userLikedPost) {
+			await Post.updateOne({ id: postId }, { $pull: { likes: userId } });
+			return res.status(200).json({ message: "Post unliked successfully" });
+		} else {
+			post.likes.push(userId);
+			await post.save();
+			res.status(200).json({ message: "Post liked successfully" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: error.message }); //Internal server error
+		console.log("Error in likeUnlikePost: ", error.message);
+	}
+};
 
 module.exports = {
 	createPost,
-    getPost,
-    deletePost,
-    likeUnlikePost
+	getPost,
+	deletePost,
+	likeUnlikePost,
 };
