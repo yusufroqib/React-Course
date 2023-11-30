9f63cf78ae3548466ec5b6dc9c6fa94e8c3909fa
yusufroqib <yusufroqib@gmail.com>
2023-11-30 15:10:15 +0100

    update

--- threads-social-clone/threads-backend/controllers/postControllers.js
+++ threads-social-clone/threads-backend/controllers/postControllers.js
@@ -82,7 +82,7 @@ const likeUnlikePost = async (req, res) => {
 		const userLikedPost = post.likes.includes(userId);
 
 		if (userLikedPost) {
-			await Post.updateOne({ id: postId }, { $pull: { likes: userId } });
+			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
 			return res.status(200).json({ message: "Post unliked successfully" });
 		} else {
 			post.likes.push(userId);
@@ -95,9 +95,38 @@ const likeUnlikePost = async (req, res) => {
 	}
 };
 
+const replyToPost = async (req, res) => {
+	try {
+		const { text } = req.body;
+		const postId = req.params.id;
+		const userId = req.user._id;
+		const userProfilePic = req.user.profilePic;
+		const username = req.user.username;
+
+		if (!text) {
+			return res.status(400).json({ message: "Text field is required" });
+		}
+
+		const post = await Post.findById(postId);
+
+		if (!post) {
+			return res.status(404).json({ message: "Post not found" });
+		}
+
+		post.replies.push({ userId, text });
+		await post.save();
+
+		res.status(200).json({ message: "Reply added successfully" });
+	} catch (error) {
+		res.status(500).json({ message: error.message }); //Internal server error
+		console.log("Error in replyToPost: ", error.message);
+	}
+};
+
 module.exports = {
 	createPost,
 	getPost,
 	deletePost,
 	likeUnlikePost,
+	replyToPost,
 };
--- threads-social-clone/threads-backend/routes/postRoutes.js
+++ threads-social-clone/threads-backend/routes/postRoutes.js
@@ -1,12 +1,19 @@
-const express = require("express")
-const { createPost, getPost, deletePost, likeUnlikePost } = require("../controllers/postControllers")
-const protectRoute = require("../middleware/protectRoutes")
+const express = require("express");
+const {
+	createPost,
+	getPost,
+	deletePost,
+	likeUnlikePost,
+	replyToPost,
+} = require("../controllers/postControllers");
+const protectRoute = require("../middleware/protectRoutes");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/:id", getPost)
-router.post("/create", protectRoute, createPost)
-router.delete("/:id",protectRoute, deletePost)
-router.post("/like/:id",protectRoute, likeUnlikePost)
+router.get("/:id", getPost);
+router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/reply/:id", protectRoute, replyToPost);
 
-module.exports = router
+module.exports = router;
