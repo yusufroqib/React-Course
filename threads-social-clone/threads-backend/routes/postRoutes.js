b3e884679fdee45f7fd9a0b360cbf62759b7f874
yusufroqib <yusufroqib@gmail.com>
2023-11-30 11:42:49 +0100

    update

--- threads-social-clone/threads-backend/routes/postRoutes.js
+++ threads-social-clone/threads-backend/routes/postRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const { createPost } = require("../controllers/postControllers")
+const protectRoute = require("../middleware/protectRoutes")
 
 const router = express.Router()
 
-router.post("/create", createPost)
+router.post("/create", protectRoute, createPost)
 
 module.exports = router
