13934f5e72b9f8abab9c74be364c9a9a6eca7727
yusufroqib <yusufroqib@gmail.com>
2023-11-30 12:06:39 +0100

    update

--- threads-social-clone/threads-backend/controllers/postControllers.js
+++ threads-social-clone/threads-backend/controllers/postControllers.js
@@ -33,7 +33,20 @@ const createPost = async (req, res) => {
 	}
 };
 
-const getPost = async (req, res) => {}
+const getPost = async (req, res) => {
+    try {
+		const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+		res.status(200).json({ post });
+	} catch (error) {
+		res.status(500).json({ message: error.message }); //Internal server error
+		console.log("Error in Get Post: ", error.message);
+	}
+
+}
 
 module.exports = {
 	createPost,
