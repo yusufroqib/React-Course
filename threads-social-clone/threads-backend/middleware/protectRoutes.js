f27f32e765bb4892ace9900fd9318a4c2e1bd768
yusufroqib <yusufroqib@gmail.com>
2023-12-01 11:22:27 +0100

    update

--- threads-social-clone/threads-backend/middleware/protectRoutes.js
+++ threads-social-clone/threads-backend/middleware/protectRoutes.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 const protectRoute = async (req, res, next) => {
-
 	try {
 		const token = req.cookies.jwt;
 		if (!token) return res.status(401).json({ message: "Unauthorized" });
