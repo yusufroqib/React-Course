bab5db71668ca3c367f656414ef970cc4f3fb827
yusufroqib <yusufroqib@gmail.com>
2023-12-01 11:29:11 +0100

    update

--- threads-social-clone/threads-backend/db/connectDB.js
+++ threads-social-clone/threads-backend/db/connectDB.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-        console.log("MongoDB connected 🎈🎈");
-    } catch (err) {
-        console.error(err);
-        process.exit(1) // exit with failure
-    }
-}
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		});
+		console.log("MongoDB connected 🎈🎈");
+	} catch (err) {
+		console.error(err);
+		process.exit(1); // exit with failure
+	}
+};
 
 export default connectDb;
