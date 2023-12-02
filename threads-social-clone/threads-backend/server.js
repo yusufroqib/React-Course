95719195794cc6dfefa9ab74ee824a50d724cb02
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2023-12-02 19:42:36 +0100

    Update server.js

--- threads-social-clone/threads-backend/server.js
+++ threads-social-clone/threads-backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
 
 dotenv.config();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
