66298cfcbb989a51e8b963584eb980a7cddc8182
yusufroqib <yusufroqib@gmail.com>
2023-12-04 11:38:25 +0100

    update

--- threads-social-clone/threads-socio/src/main.jsx
+++ threads-social-clone/threads-socio/src/main.jsx
@@ -3,42 +3,43 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import {mode } from "@chakra-ui/theme-tools"
-import { extendTheme } from '@chakra-ui/theme-utils'
+import { mode } from "@chakra-ui/theme-tools";
+import { extendTheme } from "@chakra-ui/theme-utils";
 import { BrowserRouter } from "react-router-dom";
-
+import { RecoilRoot } from "recoil";
 
 const styles = {
-   global: (props) => ({
-      body: {
-         color: mode("gray.800", "whiteAlpha: 900")(props),
-         bg: mode("gray.100", "#101010")(props),
-      },
-   }),
+	global: (props) => ({
+		body: {
+			color: mode("gray.800", "whiteAlpha: 900")(props),
+			bg: mode("gray.100", "#101010")(props),
+		},
+	}),
 };
 
 const config = {
-   initialColorMode: "dark",
-   useSystemColorMode: true,
+	initialColorMode: "dark",
+	useSystemColorMode: true,
 };
 
 const colors = {
-  gray: {
-    light: '#616161',
-    dark: '#1e1e1e'
-  }
-}
+	gray: {
+		light: "#616161",
+		dark: "#1e1e1e",
+	},
+};
 
-const theme = extendTheme({config, styles, colors})
-
+const theme = extendTheme({ config, styles, colors });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-   <React.StrictMode>
-      <BrowserRouter>
-         <ChakraProvider theme={theme}>
-            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-            <App />
-         </ChakraProvider>
-      </BrowserRouter>
-   </React.StrictMode>
+	<React.StrictMode>
+		<RecoilRoot>
+			<BrowserRouter>
+				<ChakraProvider theme={theme}>
+					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+					<App />
+				</ChakraProvider>
+			</BrowserRouter>
+		</RecoilRoot>
+	</React.StrictMode>
 );
