4d528e89327cd6430374b01b6f0a88cbdf1d47e5
yusufroqib <yusufroqib@gmail.com>
2023-12-04 12:18:20 +0100

    update

--- threads-social-clone/threads-socio/vite.config.js
+++ threads-social-clone/threads-socio/vite.config.js
@@ -5,7 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+
+    //Get rid of the CORS errors
+    proxy: {
+      "/api" : {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
 
