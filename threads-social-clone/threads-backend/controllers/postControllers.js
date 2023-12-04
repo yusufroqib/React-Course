6811a9257038a83b2c5297761d044e2a58993603
yusufroqib <yusufroqib@gmail.com>
2023-12-04 09:17:59 +0100

    update

--- threads-social-clone/threads-backend/controllers/postControllers.js
+++ threads-social-clone/threads-backend/controllers/postControllers.js
@@ -144,6 +144,20 @@ const getFeedPost = async (req, res) => {
 	}
 };
 
+const getUserPost = async (req, res) => {
+	const {username} = req.params;
+	try {
+		const user = await User.findOne({username});
+		if(!user){
+			return res.status(404).json({message: "User not found"});
+		}
+	} catch (error) {
+		res.status(500).json({message: error.message});
+		console.log("Error in getUserPost: ", error.message);
+
+	}
+}
+
 module.exports = {
 	createPost,
 	getPost,
