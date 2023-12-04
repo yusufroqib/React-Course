6c105a8a7bd35a040a4920eaa096e410f3c7638d
yusufroqib <yusufroqib@gmail.com>
2023-12-04 12:52:19 +0100

    update

--- threads-social-clone/threads-socio/src/hooks/useShowToast.js
+++ threads-social-clone/threads-socio/src/hooks/useShowToast.js
@@ -3,5 +3,19 @@ import { useCallback } from "react"
 
 const useShowToast = () => {
     const toast = useToast()
-    
+    const showToast = useCallback(
+        (title, description, status) => {
+            toast({
+                title,
+                description,
+                status,
+                duration: 3000,
+                isClosable: true,
+            })
+        },
+        [toast]
+    )
+    return showToast
 }
+
+export default useShowToast
