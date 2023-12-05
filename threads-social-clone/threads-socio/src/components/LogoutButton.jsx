72da31e20fd2dc5ae4ea39ae26f415ec8778e051
yusufroqib <yusufroqib@gmail.com>
2023-12-05 09:18:01 +0100

    update

--- threads-social-clone/threads-socio/src/components/LogoutButton.jsx
+++ threads-social-clone/threads-socio/src/components/LogoutButton.jsx
@@ -1,11 +1,24 @@
 import { Button } from '@chakra-ui/react'
 import React from 'react'
-import { FiLogout } from 'react-icons/fi'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { useSetRecoilState } from 'recoil'
+import userAtom from '../atoms/userAtom'
 
 const LogoutButton = () => {
+    const setUser = useSetRecoilState(userAtom)
+    const showToast = useShowToast()
+    const handleLogout = async () => {
+        try {
+            const res = await fetch("/api/users/logout", {
+                
+            })
+        } catch (error) {
+            
+        }
+    }
   return (
     <Button position={"fixed"} top={"30px"} right={"30px"} size={"sm"}>
-        <FiLogout size={20} />
+        <AiOutlineLogout size={20} />
     </Button>
   )
 }
