77ece23bd9cb4fbfe4e0d3a3b4e97269b2ed1aca
yusufroqib <yusufroqib@gmail.com>
2023-12-05 15:54:32 +0100

    update

--- threads-social-clone/threads-backend/routes/userRoutes.js
+++ threads-social-clone/threads-backend/routes/userRoutes.js
@@ -16,6 +16,6 @@ router.post("/signup", signUpUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.post("/follow/:id", protectRoute, followUnFollowUser); //toggle state(follow/unfollow)
-router.post("/update/:id", protectRoute, updateUser); //UpdateUser
+router.put("/update/:id", protectRoute, updateUser); //UpdateUser
 
 module.exports = router;
