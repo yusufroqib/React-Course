b45c1b6dc6db32d3c699612b0132d4413d5020da
yusufroqib <yusufroqib@gmail.com>
2023-12-05 09:09:37 +0100

    update

--- threads-social-clone/threads-socio/src/components/LogoutButton.jsx
+++ threads-social-clone/threads-socio/src/components/LogoutButton.jsx
@@ -1,8 +1,12 @@
+import { Button } from '@chakra-ui/react'
 import React from 'react'
+import { FiLogout } from 'react-icons/fi'
 
 const LogoutButton = () => {
   return (
-    <div>LogoutButton</div>
+    <Button position={"fixed"} top={"30px"} right={"30px"} size={"sm"}>
+        <FiLogout size={20} />
+    </Button>
   )
 }
 
