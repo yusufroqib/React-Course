8b9cd8b7bdcb92ee02c313caa84b046b3e88c37c
yusufroqib <yusufroqib@gmail.com>
2023-12-06 15:56:24 +0100

    update

--- threads-social-clone/threads-socio/src/components/CreatePosts.jsx
+++ threads-social-clone/threads-socio/src/components/CreatePosts.jsx
@@ -1,13 +1,48 @@
 import { AddIcon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
+import {
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+	Button,
+	useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
 
 const CreatePosts = () => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
 	return (
 		<>
-			<Button pos={"fixed"} bottom={10} right={10} leftIcon={<AddIcon />} bg={"gray"}>
+			<Button
+				pos={"fixed"}
+				bottom={10}
+				right={10}
+				leftIcon={<AddIcon />}
+				bg={"gray"}
+                onClick={onOpen}
+			>
 				Post
 			</Button>
+			<Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>Modal Title</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+                            Here we go
+					</ModalBody>
+
+					<ModalFooter>
+						<Button colorScheme="blue" mr={3} onClick={onClose}>
+							Close
+						</Button>
+						<Button variant="ghost">Secondary Action</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
 		</>
 	);
 };
