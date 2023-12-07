703d4af55748d1a78fc58a4836d63b2200cc1ca8
yusufroqib <yusufroqib@gmail.com>
2023-12-07 14:35:04 +0100

    update

--- threads-social-clone/threads-backend/controllers/postControllers.js
+++ threads-social-clone/threads-backend/controllers/postControllers.js
@@ -9,7 +9,7 @@ const createPost = async (req, res) => {
 		if (!postedBy && !text) {
 			return res
 				.status(400)
-				.json({ message: "postedBy and text fields are required." });
+				.json({ error: "postedBy and text fields are required." });
 		}
 
 		const user = await User.findById(postedBy);
