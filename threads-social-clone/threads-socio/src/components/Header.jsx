77efd00bb59576ed37160c3e35941767f96a1924
yusufroqib <yusufroqib@gmail.com>
2023-12-07 14:56:26 +0100

    update

--- threads-social-clone/threads-socio/src/components/Header.jsx
+++ threads-social-clone/threads-socio/src/components/Header.jsx
@@ -1,31 +1,35 @@
 import { Flex, Image, useColorMode, Link } from "@chakra-ui/react";
 import { useRecoilValue } from "recoil";
 import userAtom from "../atoms/userAtom";
-import {AiFillHome} from "react-icons/ai"
+import { AiFillHome } from "react-icons/ai";
 import { Link as RouterLink } from "react-router-dom";
 // import RxAvatar from "./RxAvatar";
 
 const Header = () => {
-   const { colorMode, toggleColorMode } = useColorMode();
-   const user = useRecoilValue(userAtom);
+	const { colorMode, toggleColorMode } = useColorMode();
+	const user = useRecoilValue(userAtom);
 
-   return (
-      <Flex justifyContent={"center"} mt={6} mb="12">
-         {user && (
-            <Link as={RouterLink} to="/">
-               <AiFillHome fontSize={30} />
-
-            </Link>
-         )}
-         <Image
-            cursor="pointer"
-            alt="logo"
-            w={6}
-            src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-            onClick={toggleColorMode}
-         />
-      </Flex>
-   );
+	return (
+		<Flex justifyContent={"center"} mt={6} mb="12">
+			{user && (
+				<Link as={RouterLink} to="/">
+					<AiFillHome fontSize={24} />
+				</Link>
+			)}
+			{!user && (
+				<Link as={RouterLink} to="/auth" onClick={() => setAuthScreen("login")}>
+					Login
+				</Link>
+			)}
+			<Image
+				cursor="pointer"
+				alt="logo"
+				w={6}
+				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+				onClick={toggleColorMode}
+			/>
+		</Flex>
+	);
 };
 
 export default Header;
