2378d77c71ade04c6991d16e75d729286f1940d6
yusufroqib <yusufroqib@gmail.com>
2023-12-08 10:22:03 +0100

    update

--- threads-social-clone/threads-socio/src/pages/HomePage.jsx
+++ threads-social-clone/threads-socio/src/pages/HomePage.jsx
@@ -1,26 +1,30 @@
-import { Button, Flex } from '@chakra-ui/react'
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Button, Flex } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
 
 const HomePage = () => {
+	const showToast = useShowToast();
 
-  useEffect(() => {
-    const getFeedPosts = async () => {
-      const res = await fetch("/api/posts/feed")
-    }
-   
-  }, [])
-  
+	useEffect(() => {
+		const getFeedPosts = async () => {
+			try {
+				const res = await fetch("/api/posts/feed");
+			} catch (err) {
+				showToast("Error", err.message, "error");
+			}
+		};
+	}, []);
 
-  return (
-    <div>
-      <Link to={"/markzuckerberg"}>
-        <Flex w={"full"} justifyContent={"center"}>
-          <Button mx={"auto"}>Visit Profile Page</Button>
-        </Flex>
-      </Link>
-    </div>
-  )
-}
+	return (
+		<div>
+			<Link to={"/markzuckerberg"}>
+				<Flex w={"full"} justifyContent={"center"}>
+					<Button mx={"auto"}>Visit Profile Page</Button>
+				</Flex>
+			</Link>
+		</div>
+	);
+};
 
-export default HomePage
+export default HomePage;
