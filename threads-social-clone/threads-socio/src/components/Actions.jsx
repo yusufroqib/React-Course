ca4816df12c02321079e6fc7f249ba75aff6c893
yusufroqib <yusufroqib@gmail.com>
2023-12-11 10:27:01 +0100

    update

--- threads-social-clone/threads-socio/src/components/Actions.jsx
+++ threads-social-clone/threads-socio/src/components/Actions.jsx
@@ -1,11 +1,12 @@
 import { Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { useRecoilValue } from "recoil";
 import userAtom from "../atoms/userAtom";
 
 const Actions = ({ post: post_}) => {
    const user = useRecoilValue(userAtom)
    const [liked, setLiked] = useState(post.likes.includes(user?._id))
+   const [post, setPost] = useState(post_)
 
    return (
       <>
