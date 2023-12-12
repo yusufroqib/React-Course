9e40645d9f8c30cb545e4bb1d1d9e82d9831616d
yusufroqib <yusufroqib@gmail.com>
2023-12-12 10:06:57 +0100

    update

--- threads-social-clone/threads-socio/src/pages/PostPage.jsx
+++ threads-social-clone/threads-socio/src/pages/PostPage.jsx
@@ -1,48 +1,67 @@
 import {
-  Avatar,
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Image,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuGroup,
-  MenuItem,
-  MenuList,
-  Text,
+	Avatar,
+	Box,
+	Button,
+	Divider,
+	Flex,
+	Image,
+	Menu,
+	MenuButton,
+	MenuDivider,
+	MenuGroup,
+	MenuItem,
+	MenuList,
+	Text,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsThreeDots } from "react-icons/bs";
 import Actions from "../components/Actions";
 import Comments from "../components/Comments";
 
 const PostPage = () => {
-  // const [liked, setLiked] = useState(false);
-  return (
-    <>
-      <Flex
-        w={"full"}
-        gap={3}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-      >
-        <Flex alignItems={"center"}>
-          <Avatar src="/aliumusa.jpeg" name="Aliu Musa" size={"md"} mr={2} />
-          <Text fontSize={"sm"}>Aliu Musa</Text>
-          <Image src="/verified.png" h={4} w={4} ml={2} />
-        </Flex>
-        <Flex alignItems={"center"} gap={4}>
-          <Text
-            fontSize={{ base: "xs", md: "sm" }}
-            textAlign={"right"}
-            w={36}
-            color={"gray.light"}
-          >
-            2day
-          </Text>
-          {/* <Menu>
+	// const [liked, setLiked] = useState(false);
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		const getUser = async () => {
+			try {
+				const res = await fetch(`api/users/profile/${username}`);
+				const data = await res.json();
+				if (data.error) {
+					showToast("Error", data.error, "error");
+					return;
+				}
+				setUser(data);
+			} catch (error) {
+				showToast("Error", error, "error");
+			}
+		};
+		getUser();
+	}, []);
+
+	return (
+		<>
+			<Flex
+				w={"full"}
+				gap={3}
+				alignItems={"center"}
+				justifyContent={"space-between"}
+			>
+				<Flex alignItems={"center"}>
+					<Avatar src="/aliumusa.jpeg" name="Aliu Musa" size={"md"} mr={2} />
+					<Text fontSize={"sm"}>Aliu Musa</Text>
+					<Image src="/verified.png" h={4} w={4} ml={2} />
+				</Flex>
+				<Flex alignItems={"center"} gap={4}>
+					<Text
+						fontSize={{ base: "xs", md: "sm" }}
+						textAlign={"right"}
+						w={36}
+						color={"gray.light"}
+					>
+						2day
+					</Text>
+					{/* <Menu>
             <MenuButton>
               <BsThreeDots cursor={"pointer"} onClick={e => e.preventDefault()} />
             </MenuButton>
@@ -61,47 +80,47 @@ const PostPage = () => {
               </MenuGroup>
             </MenuList>
           </Menu> */}
-        </Flex>
-      </Flex>
+				</Flex>
+			</Flex>
 
-      <Text my={3}>Hello gbogbo aye!!!</Text>
+			<Text my={3}>Hello gbogbo aye!!!</Text>
 
-      <Box
-        overflow={"hidden"}
-        borderRadius={6}
-        border={"1px solid"}
-        borderColor={"gray.light"}
-      >
-        <Image src="/post1.png" width={"full"} />
-      </Box>
-      <Flex>
-        <Actions liked={liked} setLiked={setLiked} />
-      </Flex>
-      <Flex gap={2} color={"gray.light"} fontSize={"sm"} alignItems={"center"}>
-        <Text>20 replies</Text>
-        <Box w={0.5} h={0.5} bg={"gray.light"} borderRadius={"full"}></Box>
-        <Text>{21 + (liked ? 1 : 0)} likes</Text>
-      </Flex>
+			<Box
+				overflow={"hidden"}
+				borderRadius={6}
+				border={"1px solid"}
+				borderColor={"gray.light"}
+			>
+				<Image src="/post1.png" width={"full"} />
+			</Box>
+			<Flex>
+				<Actions liked={liked} setLiked={setLiked} />
+			</Flex>
+			<Flex gap={2} color={"gray.light"} fontSize={"sm"} alignItems={"center"}>
+				<Text>20 replies</Text>
+				<Box w={0.5} h={0.5} bg={"gray.light"} borderRadius={"full"}></Box>
+				<Text>{21 + (liked ? 1 : 0)} likes</Text>
+			</Flex>
 
-      <Divider my={4} />
-      <Flex justifyContent={"space-between"}>
-        <Flex alignItems={"center"} gap={2}>
-          <Text fontSize={"2xl"}>👋</Text>
-          <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
-        </Flex>
-        <Button>Get</Button>
-      </Flex>
-      <Divider my={4} />
-
+			<Divider my={4} />
+			<Flex justifyContent={"space-between"}>
+				<Flex alignItems={"center"} gap={2}>
+					<Text fontSize={"2xl"}>👋</Text>
+					<Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+				</Flex>
+				<Button>Get</Button>
+			</Flex>
+			<Divider my={4} />
+			{/* 
       <Comments
         comment={"This is amazing"}
         createdAt={"2 min ago"}
         userAvater={"https://bit.ly/ryan-florence"}
         likes={12}
         username={"JohnDoe"}
-      />
-    </>
-  );
+      /> */}
+		</>
+	);
 };
 
 export default PostPage;
