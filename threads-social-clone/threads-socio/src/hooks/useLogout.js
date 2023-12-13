a709e2edc79faef15050bce7d4ba5bebc03658d6
yusufroqib <yusufroqib@gmail.com>
2023-12-13 11:28:26 +0100

    update

--- threads-social-clone/threads-socio/src/hooks/useLogout.js
+++ threads-social-clone/threads-socio/src/hooks/useLogout.js
@@ -1,33 +1,33 @@
-import useShowToast from './useShowToast'
-import { useSetRecoilState } from 'recoil'
-import userAtom from '../atoms/userAtom'
+import useShowToast from "./useShowToast";
+import { useSetRecoilState } from "recoil";
+import userAtom from "../atoms/userAtom";
 
 const useLogout = () => {
-    const setUser = useSetRecoilState(userAtom)
-    const showToast = useShowToast()
+	const setUser = useSetRecoilState(userAtom);
+	const showToast = useShowToast();
 
-    const logout = async () => {
-        try {
-            const res = await fetch("/api/users/logout", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
+	const logout = async () => {
+		try {
+			const res = await fetch("/api/users/logout", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
 
-            const data = await res.json()
+			const data = await res.json();
 
-            if(data.error) {
-                showToast("Error", data.error, "error")
-                return;
-            }
-            localStorage.removeItem("user-threads")
-            setUser(null)
-        } catch (error) {
-            showToast("Error", error, "error")
-        }
-    }
-    return logout
-}
+			if (data.error) {
+				showToast("Error", data.error, "error");
+				return;
+			}
+			localStorage.removeItem("user-threads");
+			setUser(null);
+		} catch (error) {
+			showToast("Error", error, "error");
+		}
+	};
+	return logout;
+};
 
-export default useLogout
+export default useLogout;
